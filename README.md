bffdc65c058a9e5256c55c81888ff17f917925ed
Josephine <jos.a.cornelius@gmail.com>
2018-08-13 16:16:00 -0500

    more accurate instructions in README and added .gitignore

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # TwitterApp NodeJS Version
 
 ## Running NodeJS Version
+Inside the onboarding-ui folder:
 ```
-node main.js
+node src/js/main.js
 ```
 And index.html can be viewed at http://localhost:9000
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
+
 var http = require('http');
 var fs = require('fs');
 
-var html = fs.readFileSync('../index.html');
+var html = fs.readFileSync('src/index.html');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
