fb5080f464096e4120a5d2bb7a7db314cbcb4f50
Josephine <jos.a.cornelius@gmail.com>
2018-08-16 15:49:46 -0500

    prints timeline

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -1,41 +1,16 @@
-// var express = require('express');
-// var app = express();
-// var cors = require('cors');
-// app.use(cors());
-// function getTimeline(){
-// 	var jXHR = $.ajax({
-// 	type: 'GET',
-// 	datatype: 'jsonp',
-// 	cors: true,
-// 	contentType:'application/json',
-// 	url: '',
-// 	data: JSON.stringify(somejson),
-// 	async: false,
-// 	secure: true,
-// 		headers: {
-// 			'Access-Control-Allow-Origin':'*',
-// 		},        
-//     success: function (response) {
-//         var resp = JSON.stringify(response)
-//         alert(resp.status);
-//     },
-//     error: function (xhr, status) {
-//         alert("error");
-//     }	
-// 	});
-// 	// return jqXHR.responseText; 
-// 	doucment.getElementById("timelinePlaceholder").innerHTML = jqXHR.responseText;
-// };
-
 function getTimeline(){
 	var xhttp = new XMLHttpRequest();
 	var URL = "http://localhost:8080/api/1.0/twitter/timeline";
     xhttp.onreadystatechange = function() {
          if (this.readyState == 4 && this.status == 200) {
-             alert(this.responseText);
+            document.getElementById('timelinePlaceholder').innerHTML = this.responseText; 
+         }
+         else{
+         	dcoument.getElementById('timelinePlaceholder').innerHTML = "There was a problem on the server side, please try again later.";
          }
     };
     xhttp.open("GET", URL, true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    document.getElementById("timelinePlaceholder").innerHTML = xhttp.send();
+    
+    xhttp.send();
 }
