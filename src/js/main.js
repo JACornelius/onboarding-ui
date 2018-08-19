cd5dccc6efbb442faf1b7c9884e2ee65a1b1fb82
Josephine <jos.a.cornelius@gmail.com>
2018-08-18 21:27:19 -0500

    added package.json, timeline loads from the front end

--- src/js/main.js
+++ src/js/main.js
@@ -6,32 +6,13 @@ var request = require('request');
 
 
 app.get('/', function(req, res){
-	fs.readFile('src/index.html', function read(err, data){
-		console.log("reading index.htmls");
-		if(err){
-			throw err;
-		}
-		res.write(data);
-	});
-	request('http://localhost:8080/api/1.0/twitter/timeline', function(err, resp, body){
-		if(resp.statusCode == 200){ 
-     		res.write(body);
-		}
-		else{
-			console.log('statusCode:', resp && resp.statusCode);
-			console.log('error:', err); 
-			res.write("There was a problem on the server, please try again later.")
-		}
-		
-     });
-
-	
+	res.sendFile(path.join(__dirname + '/../index.html'));
 });
 
 app.get('/js/timeline.js', function(req, res){
 	res.set('Content-Type', 'text/javascript');
 	fs.readFile('./src/js/timeline.js', function read(err, data){
-		console.log("reading timeline.js");
+	
 		if(err){
 			throw err;
 		}
--- src/js/timeline.js
+++ src/js/timeline.js
@@ -3,8 +3,11 @@ function getTimeline(){
 	var xhttp = new XMLHttpRequest();
 	var URL = "http://localhost:8080/api/1.0/twitter/timeline";
     xhttp.onreadystatechange = function() {
-         if (this.readyState == 4 && this.status == 200) {
+         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             document.getElementById('timelinePlaceholder').innerHTML = this.responseText; 
+         }
+         else if(this.readyState == 3){
+         	document.getElementById('timelinePlaceholder').innerHTML = "Please wait, retrieving Twitter timeline";
          }
          else{
          	document.getElementById('timelinePlaceholder').innerHTML = "There was a problem on the server side, please try again later.";
@@ -12,6 +15,6 @@ function getTimeline(){
     };
     xhttp.open("GET", URL, true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    
+   
     xhttp.send();
 }
