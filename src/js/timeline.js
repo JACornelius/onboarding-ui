9b7ecab9f56096d3cdb23770990efe36c41a600d
Josephine <jos.a.cornelius@gmail.com>
2018-08-20 13:28:19 -0500

    readable date, div element for each post

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -2,9 +2,28 @@ function getTimeline(){
 	
 	var xhttp = new XMLHttpRequest();
 	var URL = "http://localhost:8080/api/1.0/twitter/timeline";
+	var date;
+	var para;
+	var div;
+	var element;
     xhttp.onreadystatechange = function() {
          if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            document.getElementById('timelinePlaceholder').innerHTML = this.responseText; 
+         
+         	element = document.getElementById('timelinePlaceholder');
+         	element.innerHTML = "\n";
+          	var parsedJSON = JSON.parse(this.response);
+          	var element = document.getElementById('timelinePlaceholder');
+            for(var i = 0; i < parsedJSON.length; i++){
+              	div = document.createElement('div');
+            	para = document.createElement("p");
+            	var message = document.createTextNode(parsedJSON[i].message + "\n");
+            	var date = document.createTextNode(new Date(parsedJSON[i].createdAt));
+            	para.appendChild(message);
+            	para.appendChild(date);
+            	div.append(para);
+            	element.appendChild(div)
+            }
+            
          }
          else{
          	document.getElementById('timelinePlaceholder').innerHTML = "There was a problem on the server side, please try again later.";
