baa94be189b9064926104d4462f67dfe41517ec8
Josephine <jos.a.cornelius@gmail.com>
2018-08-23 21:41:05 -0500

    moved script to head

--- src/js/react.js
+++ src/js/react.js
@@ -1,12 +1,20 @@
-
-
 class Hello extends React.Component {
   render() {
     return React.createElement('p', null, `Hello ${this.props.toWhat}`);
   }
 }
+function run(){
+	ReactDOM.render(
+  	React.createElement(Hello, {toWhat: 'React'}, null),
+  	document.getElementById('reactTest')
+	);
+}
 
-ReactDOM.render(
-  React.createElement(Hello, {toWhat: 'React'}, null),
-  document.getElementById('reactTest')
-);
+const loadedStates = ['complete', 'loaded', 'interactive'];
+
+if (loadedStates.includes(document.readyState) && document.body){
+	run();
+}
+else{
+	window.addEventListener('DOMContentLoaded', run, false);
+}
