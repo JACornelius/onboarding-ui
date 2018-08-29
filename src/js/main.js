d9d48c4d529b0bdf29cf5d7160b8be1e5cc881d9
Josephine <jos.a.cornelius@gmail.com>
2018-08-29 15:43:36 -0500

    fixed it so that there is only one reactDOM.render call

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,12 @@
-import {getTimeline} from 'timeline.js';
 import {run} from 'helloReact.js';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-const timelineButton = (onClickFunction) => {
-	return React.createElement('button', {className: 'timelineButton', onClick: onClickFunction}, 'Get Timeline')
-}; 
-
-const renderedTimeline = (rawData) => {
-
-		
+let renderedTimeline = (rawData) => {		
 	let rawTimelineData = React.createElement('div', null, rawData);
 	const monthNames = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
 	let timelineArray = [];
-	//console.log(this.props.rawData);
 	for(let i in rawData) {
 		let tweetObj = rawData[i];
 		let date = new Date(tweetObj.createdAt);
@@ -37,120 +29,63 @@ const renderedTimeline = (rawData) => {
 	
 	}
 	return React.createElement('div', null, timelineArray);
-		
-	
+
 }
 
-// class buttonAndTimeline extends React.Component {
-// 	constructor(props) 
-// 	{
-// 		super(props);
-// 		this.state = {
-// 			renderedElement: null, 
-// 			xhttpRequest: null
-// 		};
-// 		this.updateTimeline = this.updateTimeline.bind(this);
-// 		this.httpCallResponse = this.httpCallResponse.bind(this);
-// 		this.httpCallRequest = this.httpCallRequest.bind(this);
-// 	}
-
-// 	updateTimeline() {
-// 		this.setState({
-// 			xhttpRequest: this.httpCallRequest,
-// 			renderedElement: this.httpCallResponse
-// 		});
-// 	}
-
-// 	httpCallResponse () {
-// 		this.xhttpRequest.onreadystatechange = () => {
-// 			if(xhttpRequest.readyState == XMLHttpRequest.DONE && xhttpRequest.status == 200){
-			
-// 					returnElement = renderedTimeline(JSON.parse(xhttpRequest.responseText));
-			
-				 
-// 			}
-// 			else if(xhttpRequest.readyState != XMLHttpRequest.DONE){  
-				
-// 					returnElement = " ";
-			       	
-				
-// 		    }
-// 		    else{
-		    	
-// 		    		returnElement = "There was a problem on the server side, please try again later.";
-		   
-		     	
-// 		    }
-// 		    this.setState({
-// 		    	renderedElement: returnElement
-// 		    })
-			
-// 		}
-// 	}
-
-// 	httpCallRequest() {
-// 		let xhttp = new XMLHttpRequest();
-// 		let returnElement;
-// 		let URL = "http://localhost:8080/api/1.0/twitter/timeline";
-// 		this.setState({
-// 			xhttpRequest: xhttp
-// 		})
-// 		xhttp.open("GET", URL, true);
-// 		xhttp.setRequestHeader("Content-type", "application/json");
-// 	    xhttp.send();
-// 	}
-
-// 	render() {
-// 		return React.createElement('div', {}, 
-// 			[//React.createElement('div', {className: 'buttonContainer'}, timelineButton(this.updateTimeline)),
-// 			React.createElement('div', {}, this.httpCallRequest), React.createElement('div', {}, this.httpCallResponse)] 
-// 			//React.createElement('div', {id: 'timelinePlaceholder'}, getTimeline())]
-// 			);
-
-// 	} 
-	
-// }
+let timelineButton = (rawData) => {
+	return React.createElement('button', {className: 'timelineButton', onClick: rawData}, 'Get Timeline')
+}; 
 
 class timelineTest extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			rawData: []
+			rawData: [],
+			updatedTimelineFunction: null
+	
 		}
+		this.componentDidMount = this.componentDidMount.bind(this);
+		this.updateTimeline = this.updateTimeline.bind(this);
 	}
 
-	componentDidMount() {
+	httpRequest() {
 		let xhttp = new XMLHttpRequest();
 		let URL = "http://localhost:8080/api/1.0/twitter/timeline";
 		let returnElement;
 		xhttp.onreadystatechange = () => {
 			if(xhttp.readyState == XMLHttpRequest.DONE && xhttp.status == 200){
-				this.setState({
-					rawData: JSON.parse(xhttp.responseText)
-				});
-				 
+				returnElement = JSON.parse(xhttp.responseText);
 			}
-			else if(xhttp.readyState != XMLHttpRequest.DONE){  
-				this.setState({
-					rawData: " "
-				});     	
-				
+			else if(xhttp.readyState != XMLHttpRequest.DONE){ 
+				returnElement = " ";
+	
 		    }
 		    else{
-		    	this.setState({
-					rawData:"There was a problem on the server side, please try again later."
-				});
+		    	returnElement = "There was a problem on the server side, please try again later.";
+		   
 		    }
+		    this.setState({
+		    	rawData: returnElement
+		    });
 		}
 		xhttp.open("GET", URL, true);
 		xhttp.send();
-		
+	}
+
+	updateTimeline(){
+		this.httpRequest();
+		updatedTimelineFunction: renderedTimeline(this.state.rawData)
+	}
+	componentDidMount() {
+		this.httpRequest();
+	
 	}
 
 	render(){
-		console.log(this.state.rawData);
-		//console.log(renderedTimeline(this.state.rawData));
-		return React.createElement('div', {id: 'timelinePlaceholder'}, renderedTimeline(this.state.rawData));
+		return React.createElement('div', {}, 
+			[React.createElement('div', {className: 'buttonContainer'}, timelineButton(this.updateTimeline)),
+			React.createElement('div', {id: 'timelinePlaceholder'}, renderedTimeline(this.state.rawData))]);
+	 
 	}
 }
 
@@ -159,12 +94,7 @@ export default timelineTest;
 window.onload = () => {
 	
 	document.addEventListener('onload', run());
-	//let reactAndTimeline = React.createElement('div', {}, [React.createElement('div', {}, run())]);
-	let reactAndTimeline = React.createElement(timelineTest, {}, null);
-	//let timelineFunction = getTimeline();
-	//let reactAndTimeline = React.createElement('div', {}, [React.createElement('div', {}, getTimeline())]);
-	//ReactDOM.render(React.createElement(buttonAndTimeline, {}, null), document.getElementById('timelineButtonAndData'));
-	//ReactDOM.render(reactAndTimeline, document.getElementById('reactTest'));
+	let reactAndTimeline = React.createElement('div', {}, [React.createElement('div', {}, run()), React.createElement(timelineTest, {}, null)]);
 	ReactDOM.render(reactAndTimeline, document.getElementById('timelineButtonAndData'));
 
 
