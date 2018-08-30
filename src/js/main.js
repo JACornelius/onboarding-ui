5d7b60d93251bedf67fac3a2d8bcf396ce7946d2
Josephine <jos.a.cornelius@gmail.com>
2018-08-30 11:01:33 -0500

    fixed error handling when 8080 is down

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ import {renderedTimeline} from 'timeline.js';
 import {timelineButton} from 'timeline.js';
 
 
-export const httpRequest = (callback) => {
+export const getTimeline = (callback) => {
 	let xhttp = new XMLHttpRequest();
 	let URL = "http://localhost:8080/api/1.0/twitter/timeline";
 	xhttp.onreadystatechange = () => {
@@ -17,7 +17,7 @@ export const httpRequest = (callback) => {
 
 	    }
 	    else{
-	    	callback("There was a problem on the server side, please try again later.");
+	    	callback("Error");
 	   
 	    }
 	}
@@ -30,6 +30,7 @@ class TimelineTest extends React.Component {
 		super(props);
 		this.state = {
 			timeline: null
+	
 	
 		}
 		this.componentDidMount = this.componentDidMount.bind(this);
@@ -46,14 +47,27 @@ class TimelineTest extends React.Component {
 
 
 	componentDidMount() {
-		httpRequest(this.timelineCallback);
+		getTimeline(this.timelineCallback);
 	
 	}
 
 	render(){
-		return React.createElement('div', {}, 
+		if(this.state.timeline == "Error") {
+			return React.createElement('div', {}, 
+			[React.createElement('div', {className: 'buttonContainer'}, timelineButton(this.componentDidMount)),
+			React.createElement('div', {},'There was a problem on the server side, please try again later.')]);
+		}
+		else if(this.state.timeline == " ") {
+			return React.createElement('div', {}, 
+			[React.createElement('div', {className: 'buttonContainer'}, timelineButton(this.componentDidMount)),
+			React.createElement('div', {},' ')]);
+		}
+		else {
+			return React.createElement('div', {}, 
 			[React.createElement('div', {className: 'buttonContainer'}, timelineButton(this.componentDidMount)),
 			React.createElement('div', {id: 'timelinePlaceholder'}, renderedTimeline(this.state.timeline))]);
+		}
+		
 	 
 	}
 }
